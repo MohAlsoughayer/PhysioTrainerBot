a9c984982385efc27eb9339fcd55282c95a916ba
Mohammed Alsoughayer <mohs@bu.edu>
2022-12-06 05:02:57 +0300

    populated strength dataset

---
+++ src/Injury_Classification.py
@@ -0,0 +1,9 @@
+# Author: Mohammed Alsoughayer
+# Descritpion: Gather Data and save it in a csv file 
+
+# Import necessary packages
+import numpy as np
+import pandas as pd
+import math
+import seaborn as sns
+
